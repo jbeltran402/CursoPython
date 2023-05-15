b28668cc8e8c04b9653ff027eee2c5424abd1d17
jbeltran402Ikno <juan.beltran@ikno.com.co>
2023-05-15 13:21:14 -0500

    Ejercicio terminado

--- App3/Ejercicios/programa_de_texto.py
+++ App3/Ejercicios/programa_de_texto.py
@@ -19,13 +19,27 @@ for i in range(3):
             letra = input(f"Ingrese letra {i+1} \n - ")
     estado = True
 
+print("\nAnalisis del texto\n")
+
 # 1). identificar cuantas veces aprarecen las letras selccionadas
 for i in range(len(letras)):
     indice_diccionario = letras[i]
-    letras_repetidas[indice_diccionario] = texto.count(letras[i])
+    print(f"la letra {indice_diccionario} se repite {texto.count(letras[i])} veces")
 
 # 2). Cuantas palabras hay en total
-print(len(texto.split()))
+print(f"\nEl texto cuenta con {len(texto.split())} palabras\n")
 
-# 3). Cuantas palabras hay en total
+# 3). Primera letra del texto y ultima letra
+print(f"La primera letra es: {texto[0:1:]}\n")
+print(f"La ultima letra es: {texto[-1]}\n")
 
+# 4). Palabras en orden inverso
+palabras = texto.split()
+palabras.reverse()
+print(f"Las palabras en orden inverso son: {' '.join(palabras)}\n")
+
+# 3). Buscar si se encuentra la palabra Python
+if "python" in texto :
+    print("El texto cuenta con la palabra python\n")
+else :
+    print("El texto NO cuenta con la palabra python\n")
